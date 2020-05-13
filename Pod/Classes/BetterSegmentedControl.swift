280622849cfbff55848755251a5978896badd9e9
w704444178@qq.com <w704444178@qq.com>
2020-05-13 17:54:38 +0800

    1

--- Pod/Classes/BetterSegmentedControl.swift
+++ Pod/Classes/BetterSegmentedControl.swift
@@ -287,12 +287,12 @@ import UIKit
     /// - Parameters:
     ///   - index: The new index.
     ///   - animated: (Optional) Whether the change should be animated or not. Defaults to `true`.
-    public func setIndex(_ index: Int, animated: Bool = true) {
+    public func setIndex(_ index: Int, animated: Bool = true, sendEvent: Bool = true) {
         guard normalSegments.indices.contains(index) else { return }
         
         let oldIndex = self.index
         self.index = index
-        moveIndicatorViewToIndex(animated, shouldSendEvent: (self.index != oldIndex || alwaysAnnouncesValue))
+        moveIndicatorViewToIndex(animated, shouldSendEvent: (self.index != oldIndex || alwaysAnnouncesValue) && sendEvent)
     }
     
     // MARK: Animations
